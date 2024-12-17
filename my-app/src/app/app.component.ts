8ee48c2d20b9f0d23bbf2aa3f4ac65962e0baf9f
shyam-rajput1487 <shyammohan.off@gmail.com>
2024-12-17 11:17:18 +0000

    .

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
-  //standalone:true,
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,AddEmployeeComponent],
   templateUrl: './app.component.html',
   template:'<div>hello</div>',
   styleUrl: './app.component.scss'
